08638b46d6c95ab072afd3f58fe1e43597a56dc6
Deng Haijun <hbdhj@sina.com>
2016-05-15 00:52:37 +0800

    update c code lines

--- resume/codelines.js
+++ resume/codelines.js
@@ -3,9 +3,9 @@ function load() {
     var codeType = new Chart(ctx, {
         type: 'pie',
         data: {
-            // 2016-05-14 ('c', 30150), ('python', 4149), ('html', 3379), ('css', 2830), ('javascript', 1321), ('swift', 1068), ('java', 999), ('objective-c', 624), ('ajax', 600), ('ruby', 424), ('php', 245), ('go', 24)
+            // 2016-05-14 ('c', 30381), ('python', 4149), ('html', 3379), ('css', 2830), ('javascript', 1321), ('swift', 1068), ('java', 999), ('objective-c', 624), ('ajax', 600), ('ruby', 424), ('php', 245), ('go', 24)
             labels: [
-                "C/C++ 30150",
+                "C/C++ 30770",
                 "python 4149",
                 "HTML 3379",
                 "css 2830",
@@ -18,7 +18,7 @@ function load() {
             ],
             datasets: [
                 {
-                    data: [30150, 4149, 3379, 2830, 1321, 1068, 999, 624, 424, 245],
+                    data: [30770, 4149, 3379, 2830, 1321, 1068, 999, 624, 424, 245],
                     backgroundColor: [
                         "#FF6384",
                         "#36A2EB",
