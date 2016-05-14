7440e3567983d03faf7f6d7bd58a1397918071c8
Deng Haijun <hbdhj@sina.com>
2016-05-14 14:27:09 +0800

    update codeline

--- resume/codelines.js
+++ resume/codelines.js
@@ -3,24 +3,31 @@ function load() {
     var codeType = new Chart(ctx, {
         type: 'pie',
         data: {
+            // 2016-05-14 {'go': 24, 'php': 245, 'c': 29182, 'java': 999, 'html': 739, 'python': 3987, 'ajax': 600, 'swift': 312, 'javascript': 806, 'css': 2830, 'objective-c': 107, 'ruby': 424}
             labels: [
-                "C/C++ 29200",
-                "python 4072",
-                "HTML 3211",
-                "JavaScript 1276",
-                "swift 1068",
-                "Java 999"
+                "C/C++ 29182",
+                "python 3987",
+                "css 2830",
+                "Java 999",
+                "JavaScript 806",
+                "HTML 739",
+                "Ruby 424",
+                "swift 312",
+                "php 245"
             ],
             datasets: [
                 {
-                    data: [29200, 4072, 3226, 1231, 1068, 999],
+                    data: [29182, 3987, 2830, 999, 806, 739, 424, 312, 245],
                     backgroundColor: [
                         "#FF6384",
                         "#36A2EB",
                         "#FFCE56",
                         "#00FFFF",
                         "#00FF00",
-                        "#FF00FF"
+                        "#FF00FF",
+                        "#0088FF",
+                        "#00FF88",
+                        "#8800FF"
                     ],
                     hoverBackgroundColor: [
                         "#FF6384",
@@ -28,7 +35,10 @@ function load() {
                         "#FFCE56",
                         "#00FFFF",
                         "#00FF00",
-                        "#FF00FF"
+                        "#FF00FF",
+                        "#0088FF",
+                        "#00FF88",
+                        "#8800FF"
                     ]
                 }]
         },
