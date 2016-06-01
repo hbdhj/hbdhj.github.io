87a55d92df82111d3598d7ed5e23250c18f274f1
Deng Haijun <hbdhj@sina.com>
2016-06-01 21:19:52 +0800

    update go lines

--- resume/codeline_data.js
+++ resume/codeline_data.js
@@ -1,5 +1,5 @@
 var myLang = new Array('C/C++', 'Python', 'HTML', 'CSS', 'JavaScript', 'Java', 'Swift', 'Objective-C', 'ajax', 'SQL', 'Ruby', 'php', 'Go');
 
-var myLines = new Array(32674, 4151, 3396, 2830, 1334, 1060, 720, 624, 600, 523, 424, 245, 199);
+var myLines = new Array(32674, 4151, 3396, 2830, 1334, 1060, 720, 624, 600, 523, 424, 245, 235);
 
-var totalLines = 48780;
+var totalLines = 48816;
