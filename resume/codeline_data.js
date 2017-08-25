240905dfeb86d4a3fe8d27d66f5fdcf2afa8ca70
Haijun Deng <hbdhj@sina.com>
2017-08-25 22:10:24 +0600

    update at Fri Aug 25 22:10:24 XJT 2017

--- resume/codeline_data.js
+++ resume/codeline_data.js
@@ -1,3 +1,3 @@
-// update at Thu Aug 24 17:07:02 XJT 2017
+// update at Fri Aug 25 22:10:24 XJT 2017
 var myLang = new Array('C++', 'Python', 'HTML', 'C', 'CSS', 'Go', 'JavaScript', 'Java', 'Swift', 'Objective C', 'SQL', 'PHP', 'C/C++ Header', 'JSON', 'Ruby',);
-var myLines = new Array(33418, 7739, 4747, 3492, 2746, 1531, 1405, 1060, 720, 488, 479, 384, 362, 362, 235,);
+var myLines = new Array(33418, 7739, 4747, 3546, 2746, 1531, 1405, 1060, 720, 488, 479, 384, 362, 362, 235,);
